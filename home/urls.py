08bb193277830443dab6cbe6b9a3b0716ef25818
Sudesh Kamthekar <sudeshkamthekar673@gmail.com>
2025-09-20 10:12:30 +0530

    sudeshamthekar673@gmail.com

--- home/urls.py
+++ home/urls.py
@@ -1,14 +1,15 @@
 from django.urls import path
+ 
 from rest_framework.authtoken.views import obtain_auth_token
-import home
+from . import views
 urlpatterns = [
 
     
-    path('', home.home, name='home'),
-    path('notes/', home.note_list, name='note_list'),
-    path('notes/list/', home.NoteListView.as_view(), name='note_list_view'),
-    path('api/notes/', home.NoteViewSet.as_view({'get': 'list', 'post': 'create'}), name='api_notes'),
-    path('api/notes/<int:pk>/', home.NoteViewSet.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'}), name='api_note_detail'),
+    path('', views.home, name='home'),
+    path('notes/', views.note_list, name='note_list'),
+    path('notes/list/', views.NoteListView.as_view(), name='note_list_view'),
+    path('api/notes/', views.NoteViewSet.as_view({'get': 'list', 'post': 'create'}), name='api_notes'),
+    path('api/notes/<int:pk>/', views.NoteViewSet.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'}), name='api_note_detail'),
     path('api/token/', obtain_auth_token, name='api_token_auth'),
     
 
