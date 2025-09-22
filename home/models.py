f55c297185572731b7a8ee8d3e886558331aa3dd
Sudesh Kamthekar <93837077+Sudesh2244@users.noreply.github.com>
2025-09-22 21:16:50 +0530

    49735

--- home/models.py
+++ home/models.py
@@ -9,4 +9,10 @@ class Note(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.title
+     
+       return self.title
+class MenuCategory(model.Model):
+    name = models.CharField(max_length =100,unique=True)
+
+    def __str_(self):
+        return self.name
